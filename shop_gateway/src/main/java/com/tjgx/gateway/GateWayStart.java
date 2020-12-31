eafd72f4e6ae99599f8c7614729577b29da6f205
baohongjian <1qaz2wsx>
2020-12-31 17:35:08 +0800

    网关

--- shop_gateway/src/main/java/com/tjgx/gateway/GateWayStart.java
+++ shop_gateway/src/main/java/com/tjgx/gateway/GateWayStart.java
@@ -2,8 +2,16 @@ package com.tjgx.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+*@Description:
+*@Author: 鲍红建
+*@date: 2020/12/31
+*/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GateWayStart {
 
     public static void main(String[] args) {
